de7a94abe967d2e5e27b3615dd8ddd454075a322
Odebode oluwatimilehin <odebodezion@gmail.com>
2024-09-30 20:56:55 +0100

    Add cirrect path to credentials path

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -14,10 +14,12 @@ export class PostgresConfigService implements TypeOrmOptionsFactory {
                             process.env.PROD_DB :
                             process.env.DEV_DB
     async createTypeOrmOptions(): Promise<TypeOrmModuleOptions> {
-        let aivenCloudCertificate = await fs.readFile('aiven_credentials.txt', {encoding: 'utf-8'});
-        
-        let data = await fs.readFile('credentials.json', {encoding: 'utf-8'})
-        
+        const certificatePath = join(__dirname, '..', '..', 'aiven_credentials.txt')
+        let aivenCloudCertificate = await fs.readFile(certificatePath, {encoding: 'utf-8'});
+       
+        const credentialPath = join(__dirname, '..', '..', 'credentials.json'); 
+        let data = await fs.readFile(credentialPath, {encoding: 'utf-8'})
+
         let creds = new Object(JSON.parse(data))
         
         return {
